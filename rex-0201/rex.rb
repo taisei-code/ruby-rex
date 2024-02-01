c2e7533c2103f9c5e2012d7f07143e503f739ef3
Taisei <taisei96.web@gmail.com>
2024-02-02 06:31:48 +0900

    解説

--- rex-0201/rex.rb
+++ rex-0201/rex.rb
@@ -36,3 +36,49 @@
 =begin
 String#concatはselfへ引数の値を結合します。selfの内容を変更します。
 =end
+
+# No4
+
+# def hoge(step = 1)
+#   current = 0
+#   Proc.new {
+#     current += step
+#   }
+# end
+
+# p1 = hoge
+# p2 = hoge(2)
+
+# p2.call 
+# p2.call 
+
+# p p2.call
+
+=begin
+p1とp2は別のProcオブジェクトのため、hogeメソッド内のcurrent変数は共有されません。
+よって、p2の結果は6になります。
+=end
+
+# No5
+
+arr = ["c", 2, "a", 3, 1, "b"]
+arr.sort
+p arr
+
+=begin
+Arrayクラスのsortメソッドの問題です。
+
+sortメソッドは配列の要素を並び替えたい時に使用します。
+
+要素の並び替えは<=>演算子によって行われます。
+
+(要素1 <=> 要素2)の結果が-1なら要素1が順番として先に並び、0ならそのまま、1なら要素2が先に並びます。
+
+また<=>演算子で比較できない要素があると例外ArgumentErrorが発生します。
+
+<=>では文字列と数値は比較できません。
+
+問題のプログラムは文字列と数値を比較しています。
+
+よって答えは「ArgumentErrorになる」です。
+=end
