82ad3943481de96a048f82028247f74765467e34
Taisei <taisei96.web@gmail.com>
2024-02-08 06:23:30 +0900

    解説

--- rex-0207/rex.rb
+++ rex-0207/rex.rb
@@ -176,4 +176,105 @@ File.pwdメソッド
 =begin
 attr_accessorメソッド・・・引数に指定した名前のインスタンス変数に対して読み取り用のメソッド(getterメソッド)と書き込み用メソッド(setterメソッド)を定義
 引数にはStringクラスかSymbolクラスのリテラルを指定
+
+読み込みと書き込み
+=end
+
+# No13 
+
+# class Foo
+#   attr_reader :a
+  
+#   def initialize
+#     @a = "REx"
+#   end
+
+# end
+
+# foo = Foo.new
+# puts foo.a
+
+=begin
+attr_readerメソッドは引数に指定した名前のインスタンス変数に対して読み取り用メソッド(getterメソッド)を定義します。
+initialize・・・initializeメソッドが記述されているクラスからClass#newメソッドでオブジェクトを生成する際に、必ず実行される初期化メソッド
+=end
+
+# No14 保留
+
+# No15
+
+# puts '10'.oct #String#octは文字列を8進数文字列と解釈して、整数を返す。
+# puts 080 # 080はSyntaxErrorになります。
+# puts "110".to_i(2) #Fixnum#to_i(n)は文字列をn進数文字列と解釈して、整数を返します。
+
+# No16
+
+# def foo
+#   self + 2
+# end
+# p 1.foo
+
+=begin
+
+=end
+
+# No17 保留
+
+# No19
+
+# a1 = [1,2,3]
+# a2 = [4,2,3]
+
+# p a1 | a2
+
+=begin
+Array#|は集合の和結合を行います。重複する要素は取り除かれます。
+=end
+
+# No20
+
+# (x, y), z = 1, 2, 3
+# p z
+
+=begin
+多重代入の場合、変数に対して代入する値の数が少ないときは余った変数にnilが格納されます。
+変数に対して値の数が多い場合は余った値は無視されます。
+また1つの変数に対して複数の値を代入する場合は配列として代入されます。
+=end
+
+# No21 保留
+
+# No22
+
+# arr = (1..30).to_a
+# container = []
+
+# arr.each_cons(7) do |i|
+#   container << i
+# end
+# p container.length
+
+=begin
+Array#each_cons(cnt)はselfからcnt個ずつ要素を取り出しブロックに渡します。ブロック引数には配列で渡されます。
+取り出す要素は、[要素1, 要素2, 要素3], [要素2, 要素3, 要素4] ...と1つづ前に進みます。
+=end
+
+# No23
+
+# str = "1;2:3;4"
+# p str.split(";|:")
+
+# No24
+
+# p [1,2,3,4].map do |e| e * e end
+
+=begin
+
+=end
+
+# No25
+
+=begin
+Array#unshift | selfの先頭へ引数の値を破壊的に追加します(FIFO)。引数が指定されていない場合は何もしません。
+Array#shift | selfの先頭より1要素を破壊的に取り出します(FIFO)。 
 =end
