e37158b357b062a903c1d343f02700db2512c98d
Taisei <taisei96.web@gmail.com>
2024-02-10 20:24:45 +0900

    復習

--- rex-0201/README.md
+++ rex-0201/README.md
@@ -10,6 +10,39 @@ attr_readerとattr_writerを1つにまとめたメソッド
 attr_reader :nameと書けば、インスタンス変数@nameにアクセスできる。  
 しかし、**@nameをクラスの外部から変更できない**
 
+```
+class Human
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+
+human = Human.new("サラー")
+puts human.name 
+human.name = "ソボスライ" # クラスの外部から変更
+puts human.name # attr_writerが無いと、NoMethodErrorになる
+```
+
 ## attr_writerメソッド
 **クラスの外部からインスタンス変数@nameを変更できる**
+
+```
+class Human
+  attr_reader :name
+  attr_writer :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+
+human = Human.new("サラー")
+puts human.name 
+human.name = "ソボスライ" 
+puts human.name 
+```
 
--- rex-0201/review.rb
+++ rex-0201/review.rb
@@ -1,44 +1,44 @@
-# # # class User
-# # #   @name = ""
-# # # end
+# # # # class User
+# # # #   @name = ""
+# # # # end
 
-# # # user = User.new
-# # # user.name = "taro"
-# # # p user.name # NoMethodError
+# # # # user = User.new
+# # # # user.name = "taro"
+# # # # p user.name # NoMethodError
 
-# # class Human
-# #   attr_reader :name
-# #   attr_writer :name
+# class Human
+#   attr_reader :name
+#   attr_writer :name
 
-# #   def initialize(name)
-# #     @name = name
-# #   end
+#   def initialize(name)
+#     @name = name
+#   end
 
-# # end
-
-# # human = Human.new("taro")
-# # puts human.name #taro
-# # human.name = "siro" # クラスの外部から変更
-# # puts human.name # attr_writerが無いと、NoMethodErrorになる
-
-# # class Human
-# #   attr_accessor :name
-
-# #   def initialize(name)
-# #     @name = name
-# #   end
-
-# # end
-
-# # human = Human.new("taro")
-# # puts human.name
-# # human.name = "siro"
-# # puts human.name
-
-# class Foo
-#   attr_writer :a 
 # end
 
-# foo = Foo.new
-# foo.a ="REx"
-# puts foo.a # 読み取りのattr_readerが定義されてないから、NoMethodError
+# human = Human.new("taro")
+# puts human.name #taro
+# human.name = "siro" # クラスの外部から変更
+# puts human.name # attr_writerが無いと、NoMethodErrorになる
+
+# # # class Human
+# # #   attr_accessor :name
+
+# # #   def initialize(name)
+# # #     @name = name
+# # #   end
+
+# # # end
+
+# # # human = Human.new("taro")
+# # # puts human.name
+# # # human.name = "siro"
+# # # puts human.name
+
+# # class Foo
+# #   attr_writer :a 
+# # end
+
+# # foo = Foo.new
+# # foo.a ="REx"
+# # puts foo.a # 読み取りのattr_readerが定義されてないから、NoMethodError
