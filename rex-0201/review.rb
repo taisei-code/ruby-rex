8ae16a0b01b0272399d115406a61385840ef78f3
Taisei <taisei96.web@gmail.com>
2024-02-10 19:41:57 +0900

    復習

---
+++ rex-0201/review.rb
@@ -0,0 +1,7 @@
+class User
+  @name = ""
+end
+
+user = User.new
+user.name = "taro"
+p user.name # NoMethodError
--- rex-0201/rex.rb
+++ rex-0201/rex.rb
@@ -1,12 +1,12 @@
 # No1
 
-# # class Foo
-# #   attr_writer :a
-# # end
+class Foo
+  attr_writer :a
+end
 
-# # foo = Foo.new
-# # foo.a = "REx" # => オブジェクトfooが保持するインスタンス変数@aに文字列"value"が代入される
-# # puts foo.a    # => インスタンス変数@aに対して読み取り用のメソッドが定義されていないためNoMethodErrorが発生
+foo = Foo.new
+foo.a = "REx" # => オブジェクトfooが保持するインスタンス変数@aに文字列"value"が代入される
+puts foo.a    # => インスタンス変数@aに対して読み取り用のメソッドが定義されていないためNoMethodErrorが発生
 
 
 # No2
