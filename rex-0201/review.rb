e895885fad248f37245c02cf9bac6eec72c005e4
Taisei <taisei96.web@gmail.com>
2024-02-10 19:48:02 +0900

    復習

--- rex-0201/review.rb
+++ rex-0201/review.rb
@@ -5,3 +5,15 @@ end
 user = User.new
 user.name = "taro"
 p user.name # NoMethodError
+
+class Human
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+  end
+
+end
+
+human = Human.new("taro")
+puts human.name
