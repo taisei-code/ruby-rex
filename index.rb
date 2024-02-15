78fc5e62f8d2cd4c220adfe635022282140fa185
Taisei <taisei96.web@gmail.com>
2024-02-16 04:49:55 +0900

    復習

--- index.rb
+++ index.rb
@@ -12,7 +12,45 @@ p human.to_a # [[:name, "pikawaka"], [:age, 25]]
 ##範囲オブジェクト
 p (1..10).to_a # [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 
+
+## to_s
+
+num = 123
+p num.to_s
+
 ## map
 
 numbers = [1,2,3]
 p numbers.map { |n| n * 2} # [2, 4, 6]
+
+## step 
+
+# 1.step(10,2) do |x|
+#   print x # 13579
+# end
+
+## strftime
+
+require "date"
+d = Date.today.strftime("%F")
+p d # 2024-02-15
+
+# 同じ動作
+d1 = Date.today.strftime("%Y-%m-%d")
+p d1
+
+## invert
+
+p ({a: 100, b: 100}).invert # {100=>:b}
+
+## flatten
+
+array1 = [[1,2], [3,4]]
+p array1.flatten
+
+## flatten!
+
+array2 = [[1,2],[3,4]]
+# p array2.object_id # 70199722528440
+p array2.flatten! # [1, 2, 3, 4]
+p array2.object_id # 70118177214120
--- rex-0215/README.md
+++ rex-0215/README.md
@@ -6,20 +6,32 @@
 - to_a
 ハッシュ、範囲オブジェクトなどを配列に変換する
 
+- to_s
+オブジェクトを文字列にする
+
 - map
 配列の要素の数だけブロック内で処理を繰り返して、新しい配列を返す
 
-
-- scan
-
-- strip 
-
 - step
-
-- split 
+最初の数値から繰り返しのたびに毎回一定の数値を加算していき、指定した最高値を超えるまで繰り返しを行うことができます。
 
 - strftime
+日時データを指定したフォーマットで文字列に変換することができるメソッド。
+Time, Date, DateTimeクラスに対して使える
 
+- invert（Hash#invert）
+キーと値を入れ替えます。
+入れ替えの結果キーが重複した場合は、後に定義された方が優先されます。
+
+- flatten
+多次元配列を1次元配列にする
+
+- flatten!
+破壊的メソッド
+
+- scan
+- strip 
+- split 
 - zip 
 - transpose
 - product
@@ -36,10 +48,6 @@
 - find_all
 - detect
 - collect
-- invert
-- to_s
-- flatten
-- flatten!
 - strip
 - chomp
 - chop
