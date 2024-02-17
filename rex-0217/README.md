7299cff7ad55fef346d7faa7c827aa4582dc7685
Taisei <taisei96.web@gmail.com>
2024-02-17 16:16:22 +0900

    復習

---
+++ rex-0217/README.md
@@ -0,0 +1,5 @@
+## Array#each_slice(n)
+
+- selfからn個で配列を区切りブロックに渡す
+
+`self.each_slice(n)`
--- rex-0217/review.rb
+++ rex-0217/review.rb
@@ -1,5 +1,18 @@
 # 1~10
 
+# 5
+
+def hoge(step = 1)
+  current = 0
+  Proc.new {
+    current += step
+  }
+end 
+
+p1 = hoge
+p1.call
+p p1
+
 # 8 
 
 # arr = [1,2,3,4,5,6,7,8,9,10]
@@ -7,11 +20,11 @@
 #   p i
 # end
 
-arr = (1..30).to_a
-container = []
+# arr = (1..30).to_a
+# container = []
 
-arr.each_slice(7) do |i|
-  container << i
-end 
+# arr.each_slice(7) do |i|
+#   container << i
+# end 
 
-p container.length # 5
+# p container.length # 5
